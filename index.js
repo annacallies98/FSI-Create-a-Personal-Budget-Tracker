6ea95738f37c55989326b185825c4ad82a3cddd5
Anna Callies <annacallies98@gmail.com>
2022-04-01 21:02:41 -0500

    Budget tracker JavaScript

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+// let answer = Number(window.prompt(weeklyExpenseQuestions[0]))
+// console.log(answer)
+
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let answer = Number(window.prompt(weeklyExpenseQuestions[i]))
+    console.log(answer)
+    weeklyExpenses = weeklyExpenses + answer
+} 
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let answer = Number(window.prompt(monthlyExpenseQuestions[i]))
+    console.log(answer)
+    monthlyExpenses = monthlyExpenses + answer
+} 
+for (let i = 0; i < annualExpenseQuestions.length; i++) {
+    let answer = Number(window.prompt(annualExpenseQuestions[i]))
+    console.log(answer)
+    annualExpenses = annualExpenses + answer
+} 
